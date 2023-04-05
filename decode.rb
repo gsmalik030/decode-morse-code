168dd40ee69296a926890bc2125773aabb445d35
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-05 14:32:37 +0200

    Added decode_char and decode_word functions

---
+++ decode.rb
@@ -0,0 +1,44 @@
+MORSE_CODE = {
+  '.-'   => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..'  => 'D',
+  '.'    => 'E',
+  '..-.' => 'F',
+  '--.'  => 'G',
+  '....' => 'H',
+  '..'   => 'I',
+  '.---' => 'J',
+  '-.-'  => 'K',
+  '.-..' => 'L',
+  '--'   => 'M',
+  '-.'   => 'N',
+  '---'  => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.'  => 'R',
+  '...'  => 'S',
+  '-'    => 'T',
+  '..-'  => 'U',
+  '...-' => 'V',
+  '.--'  => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}
+
+def decode_char(char)
+  return MORSE_CODE[char]
+end
+
+def decode_word(word)
+  result =[]
+  letters = word.split
+  letters.map do |letter|
+    result.push(decode_char(letter))
+end
+  return result.join()
+end
+
+puts decode_char("-.--")
+puts decode_word("-- -.--")
